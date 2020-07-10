b2ad6cec5ba123ae1a7f19548365eb29227dd79e
Ben Coleman <benc.uk@gmail.com>
2020-07-10 23:55:49 +0100

    work

--- dist/index.js
+++ dist/index.js
@@ -42,8 +42,6 @@ module.exports =
 /******/ 		// Load entry module and return exports
 /******/ 		return __webpack_require__(104);
 /******/ 	};
-/******/ 	// initialize runtime
-/******/ 	runtime(__webpack_require__);
 /******/
 /******/ 	// run startup
 /******/ 	return startup();
@@ -397,26 +395,29 @@ module.exports = require("os");
 /***/ }),
 
 /***/ 104:
-/***/ (function(__unusedmodule, __webpack_exports__, __webpack_require__) {
+/***/ (function(__unusedmodule, __unusedexports, __webpack_require__) {
 
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _actions_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(470);
-/* harmony import */ var _actions_core__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_actions_core__WEBPACK_IMPORTED_MODULE_0__);
-/* harmony import */ var _actions_github__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(469);
-/* harmony import */ var _actions_github__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_actions_github__WEBPACK_IMPORTED_MODULE_1__);
-
-
+const core = __webpack_require__(470);
+const github = __webpack_require__(469);
 
 try {
-  const ref = _actions_core__WEBPACK_IMPORTED_MODULE_0___default().getInput('ref');
-  console.log(`### REF ${ref}!`);
+  const workflowId = core.getInput('workflow-id');
+  const ref = core.getInput('ref') || github.context.ref;
+  const token = core.getInput('token');
+
+  const octokit = github.getOctokit(token);
+
+  const owner = github.context.repo.owner;
+  const repo = github.context.repo.repo;
+  octokit.request(`POST repos/${owner}/${repo}/actions/workflows/${workflowId}/dispatches`, {
+    ref: ref
+  });
   
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(_actions_github__WEBPACK_IMPORTED_MODULE_1___default.a.context.payload, undefined, 2)
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
 } catch (error) {
-  _actions_core__WEBPACK_IMPORTED_MODULE_0___default().setFailed(error.message);
+  core.setFailed(error.message);
 }
 
 /***/ }),
@@ -9450,43 +9451,4 @@ function onceStrict (fn) {
 
 /***/ })
 
-/******/ },
-/******/ function(__webpack_require__) { // webpackRuntimeModules
-/******/ 	"use strict";
-/******/ 
-/******/ 	/* webpack/runtime/make namespace object */
-/******/ 	!function() {
-/******/ 		// define __esModule on exports
-/******/ 		__webpack_require__.r = function(exports) {
-/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 			}
-/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 		};
-/******/ 	}();
-/******/ 	
-/******/ 	/* webpack/runtime/compat get default export */
-/******/ 	!function() {
-/******/ 		// getDefaultExport function for compatibility with non-harmony modules
-/******/ 		__webpack_require__.n = function(module) {
-/******/ 			var getter = module && module.__esModule ?
-/******/ 				function getDefault() { return module['default']; } :
-/******/ 				function getModuleExports() { return module; };
-/******/ 			__webpack_require__.d(getter, 'a', getter);
-/******/ 			return getter;
-/******/ 		};
-/******/ 	}();
-/******/ 	
-/******/ 	/* webpack/runtime/define property getter */
-/******/ 	!function() {
-/******/ 		// define getter function for harmony exports
-/******/ 		var hasOwnProperty = Object.prototype.hasOwnProperty;
-/******/ 		__webpack_require__.d = function(exports, name, getter) {
-/******/ 			if(!hasOwnProperty.call(exports, name)) {
-/******/ 				Object.defineProperty(exports, name, { enumerable: true, get: getter });
-/******/ 			}
-/******/ 		};
-/******/ 	}();
-/******/ 	
-/******/ }
-);
+/******/ });
--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-import core from '@actions/core'
-import github from '@actions/github'
+const core = require('@actions/core');
+const github = require('@actions/github');
 
 try {
-  const ref = core.getInput('ref');
-  console.log(`### REF ${ref}!`);
+  const workflowId = core.getInput('workflow-id');
+  const ref = core.getInput('ref') || github.context.ref;
+  const token = core.getInput('token');
+
+  const octokit = github.getOctokit(token);
+
+  const owner = github.context.repo.owner;
+  const repo = github.context.repo.repo;
+  octokit.request(`POST repos/${owner}/${repo}/actions/workflows/${workflowId}/dispatches`, {
+    ref: ref
+  });
   
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
