bea4f7053f4e690e223b452d97955b55f4af5f6a
Ben Coleman <benc.uk@gmail.com>
2020-07-13 22:22:50 +0100

    adding debug info

--- src/main.ts
+++ src/main.ts
@@ -29,7 +29,9 @@ async function run(): Promise<void> {
     if(listResp.status != 200) throw new Error(`Got HTTP ${listResp.status} calling list workflows API 💩`)
 
     // Debug response if ACTIONS_STEP_DEBUG is enabled
+    core.debug('### START List Workflows response data')
     core.debug(listResp.data)
+    core.debug('### END:  List Workflows response data')
 
     // Locate workflow by name as we need it's id
     const foundWorkflow = listResp.data.workflows.find((wf: Record<string, string>) => {
